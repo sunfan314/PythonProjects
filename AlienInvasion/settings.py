3e1fafa0ae935bb8baafaee87e9a97d64a2fe69e
sunfan314 <462010622@qq.com>
2018-08-24 16:03:53 +0800

    upadte

---
+++ AlienInvasion/settings.py
@@ -0,0 +1,35 @@
+class Settings():
+	#store game settings
+	def __init__(self):
+		self.is_mac = True
+		self.screen_width = 800
+		self.screen_height = 600
+		self.bg_color = (230,230,230)
+		#ship config
+		self.ship_limit = 3
+		#bullet config
+		self.bullet_width = 2
+		self.bullet_height = 10
+		self.bullet_color = (60,80,60)
+		self.bullets_allowed = 10
+
+		#alien config
+		self.fleet_drop_speed = 10
+		self.speedup_scale = 1.3    
+		self.score_scale = 1.5   
+		self.initialize_dynamic_settings()
+	def initialize_dynamic_settings(self):
+		self.ship_speed_factor = 15 
+		self.bullet_speed_factor = 20 
+		self.alien_speed_factor = 10
+		self.fleet_direction = 1
+
+		self.alien_points = 50
+	def increase_speed(self):
+		self.ship_speed_factor *= self.speedup_scale
+		self.bullet_speed_factor *= self.speedup_scale
+		self.alien_speed_factor *= self.speedup_scale
+		self.alien_points = int(self.alien_points * self.score_scale)
+
+
+		
